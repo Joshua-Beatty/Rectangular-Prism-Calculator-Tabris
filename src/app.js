c8f08586bb7015317b3e3c2b6cbcafeeec8ea171
Josh <joshuatb6@gmail.com>
2020-05-29 11:04:55 -0700

    fixing navigationView

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
-const {TextInput, Slider, TextView, contentView, Color, Font, sizeMeasurement, NavigationView, Page, drawer, Button, Canvas} = require('tabris');
+const {TextInput, Slider, TextView, contentView, Color, Font, sizeMeasurement, NavigationView, Page, drawer, Button, Canvas, statusBar, devTools} = require('tabris');
 
 const fontSizeNavigation = '25px';
 const fontSizeNavigationTitle = '35px';
+
+
+newRect = function(attributes, color){
+	return new Canvas(attributes)
+	.onResize(({target: canvas, width, height}) => {
+		const context = canvas.getContext('2d', width, height);
+		context.fillStyle = color;
+		context.fillRect(0, 0, width, height);
+	})
+}
+
+//statusBar.background = new Color(255, 160, 0);
+statusBar.background = new Color(0, 0, 0, 0);
+statusBar.displayMode = 'float';
+statusBar.theme = 'light';
+devTools.hideUi();
 
 rectangularPrismPage = new Page({title: 'Rectangular Prism'});
 const rect = require('./rect.js');
@@ -15,35 +31,31 @@ let pages = [rectangularPrismPage, helpPage]
 
 navigation = new NavigationView({
 	layoutData: 'stretch', 
+	top: statusBar.height,
 	drawerActionVisible: true,
-	titleTextColor: new Color(255, 255, 255),
+	titleTextColor: new Color(0, 0, 0),
 	toolbarColor: new Color(255, 160, 0),
-	actionColor: new Color(255, 255, 255)
+	actionColor: new Color(0, 0, 0)
 }).appendTo(contentView);
 navigation.append(pages[0]);
 
+newRect({top:0, height:statusBar.height, right:0, left:0}, new Color(255, 160, 0)).appendTo(contentView);
+
 drawer.append(
 	new TextView({
-		centerX: true, top: '1%',
+		centerX: true, top: statusBar.height,
 		font: fontSizeNavigationTitle,
 		text: "Navigation",
 	})
 );
 
-newRect = function(attributes, color){
-	return new Canvas(attributes)
-	.onResize(({target: canvas, width, height}) => {
-		const context = canvas.getContext('2d', width, height);
-		context.fillStyle = color;
-		context.fillRect(0, 0, width, height);
-	})
-}
 
-hLAttributes = {top:'prev()', height:1, right:0, left:0};
+hLAttributes = {top:statusBar.height+navigation.toolbarHeight-1, height:1, right:0, left:0};
 hLColor = new Color(200,200,200);
 
 newRect(hLAttributes, hLColor).appendTo(drawer);
 
+hLAttributes = {top:'prev()', height:1, right:0, left:0};
 pages.forEach(function (item, index) {
 	new Button({
 		text: item.title,
