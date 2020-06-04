4e7afc262c9e63cb568fcd0ae6087482ad6afbf9
Joshua-Beatty <joshuatb6@gmail.com>
2020-06-04 13:37:17 -0700

    adding copping email support

--- src/help.js
+++ src/help.js
@@ -1,16 +1,37 @@
-const {TextInput, Slider, TextView, contentView, Color, Font, sizeMeasurement, NavigationView, Page, drawer, Button, Canvas, Composite} = require('tabris');
+const {TextInput, Slider, TextView, contentView, Color, Font, sizeMeasurement, NavigationView, Page, drawer, Button, Canvas, Composite, AlertDialog} = require('tabris');
 const fontSizes = require('./fontSizes.js');
 
 loadPage = function(newPage){
+	var text = "joshbeatty20@gmail.com";
+
+	
+
 	new TextView({
 		left: 16, right: 16, top: '1%',
 		font: fontSizes.paragraph,
 		text: "For any questions, concerns, or offers please email me at:",
 	}).appendTo(newPage);
-	new TextView({
+	new Button({
 		centerX: true, top: 'prev() 23',
 		text: "joshbeatty20@gmail.com",
 		font: fontSizes.paragraph,
+		style:'text'
+	}).onSelect(() => {
+		try {
+			cordova.plugins.clipboard.copy(text);
+	new AlertDialog({
+		title: 'Coppied email to clipboard',
+		buttons: {ok: 'OK'}
+	}).open();
+
+		}
+		catch(err) {
+	new AlertDialog({
+		title: 'Could not coppy email to clipboard',
+		buttons: {ok: 'OK'}
+	}).open();
+
+		}
 	}).appendTo(newPage);
 	new TextView({
 		centerX: true, bottom: '50%',
